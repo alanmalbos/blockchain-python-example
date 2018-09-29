78d3dc0a421ace6910684f6a86b07ea41f9892d0
Alan Malbos <alanmalbos@gmail.com>
2018-09-29 16:33:34 -0300

    Add a validate function to verify the hash of the current and previous block

--- blockchain.py
+++ blockchain.py
@@ -29,3 +29,19 @@ class Blockchain:
 		previous_block_hash = self.chain[len(self.chain) - 1].hash
 		new_block = Block(transactions, previous_block_hash)
 		self.chain.append(new_block)
+
+	# validate de hash with the atual block and the previous one
+	def validate_chain(self):
+		for i in range(1, len(self.chain)):
+			current = self.chain[i]
+			previous = self.chain[i - 1]
+			# check te current block's hash
+			if(current.hash() != current.generate_hash()):
+				print("The current block is not valid, the atual hash it is not the same!")
+				return False
+				# check the previous block's hash in then atual block
+			if(current.previous_hash != previous.generate_hash()):
+				print("The previous block's hash is invalid!")
+				return False
+			# returned true after passed in the two conditions 
+			return True
